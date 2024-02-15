94c7e9dd2f932917b3e7935bb8839780ecfb1c66
JABAN111 <368601@edu.itmo.ru>
2024-02-15 10:04:51 +0300

    изменена LocationStrategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,15 @@
-import {provideRouter, RouterModule, Routes} from "@angular/router";
-import {ApplicationConfig, NgModule} from "@angular/core";
+import {provideRouter, Routes} from "@angular/router";
+import {ApplicationConfig} from "@angular/core";
 import {provideAnimations} from "@angular/platform-browser/animations";
 
-// компоненты, которые сопоставляются с маршрутами
 import {MainPageComponent} from "./mainPage/mainPage.component";
 import {NotFoundComponent} from "./ErrorPages/notFound/not-found.component";
 import {StartPageComponent} from "./startPage/startPage.component";
 import {AccessDeniedComponent} from "./ErrorPages/access-denied/access-denied.component";
 import {AuthGuard} from "./UtilsAndServices/Utils/AuthGuard";
-import {HttpService} from "./UtilsAndServices/Services/HttpService";
-import {NgModel} from "@angular/forms";
 import {provideHttpClient} from "@angular/common/http";
+import {HashLocationStrategy, LocationStrategy} from "@angular/common";
 
-// определение маршрутов
-//for Local
 const appRoutes: Routes =[
   { path: "", component: StartPageComponent},
   { path: "main", component: MainPageComponent, canActivate: [AuthGuard]},
@@ -23,6 +19,8 @@ const appRoutes: Routes =[
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes),provideAnimations(),provideHttpClient()]
+  providers: [provideRouter(appRoutes),provideAnimations(),provideHttpClient(),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ]
 };
 
